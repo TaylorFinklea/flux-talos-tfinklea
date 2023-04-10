24e78109c3b24d0527f512b1682c03b1b10b1b71
Taylor Finklea <taylor.finklea@gmail.com>
2023-04-10 12:39:39 -0500

    update

--- scripts/sops_encrypt.sh
+++ scripts/sops_encrypt.sh
@@ -5,5 +5,5 @@ SOPS_AGE_KEY_FILE="$HOME/icloud/homelab/talos/homelab/sops/age/age.agekey"
 # Find all the *.sops.yaml files recursively in the current directory and apply the decrypt and encrypt commands to them
 find ../ -name "*.sops.yaml" -type f -print0 | while IFS= read -r -d '' file; do
 	# eval "sops --decrypt --age $(cat $SOPS_AGE_KEY_FILE | ggrep -oP '(?<=public key: )(.*)') --encrypted-regex '(^(.*[Pp]ass.*)$)|(^(data|stringData)$)|(^(.*[Tt]oken.*)$)' --in-place $file"
-	eval "sops --encrypt --age $(cat $SOPS_AGE_KEY_FILE | ggrep -oP '(?<=public key: )(.*)') --encrypted-regex '(^(.*[Pp]ass.*)$)|(^(data|stringData)$)|(^(.*[Tt]oken.*)$)|(^(.*[Kk]ey.*)$)' --in-place $file"
+	eval "sops --encrypt --age $(cat $SOPS_AGE_KEY_FILE | ggrep -oP '(?<=public key: )(.*)') --encrypted-regex '(^(.*[Pp]ass.*)$)|(^(data|stringData)$)|(^(.*[Tt]oken.*)$)|(^(.*[Kk]ey.*)$)|(^(.*PASSWORD.*)$)' --in-place $file"
 done
